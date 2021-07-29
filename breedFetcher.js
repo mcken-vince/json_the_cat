862aab044bb70753847837abeb92de7a24b88b3e
Vincent McKen <mcken.vince@gmail.com>
2021-07-29 15:51:28 +0000

    separated fetchBreedDescription function to be imported by index.js, all working

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,25 +1,28 @@
 const request = require('request');
-const breed = process.argv.slice(2)[0];
 
-request(`https://api.thecatapi.com/v1/breeds/search/?q=${breed}`, (error, response, body) => {
-  // console.log("response: ", response.statusCode); // testing output
-  if (error || response.statusCode === 404) {
-    console.log(`There has been an error: 
-    Response: ${response.statusCode}
-    Error: ${error}`);
-    process.exit();
-  } 
-  // parse into JSON object
-  const data = JSON.parse(body);
-  // console.log("JSON object returned: ", data); // testing output
+const fetchBreedDescription = (breedName, callback) => {
+  request(`https://api.thecatapi.com/v1/breeds/search/?q=${breedName}`, (err, response, body) => {
+    // console.log("response: ", response.statusCode); // testing output
+    if (err || response.statusCode === 404) {
+      callback(`There has been an error: 
+      Response: ${response.statusCode}
+      Error: ${err}`, null);
+      // passinto callback error message and null description value
+    } 
+    // parse into JSON object
+    const data = JSON.parse(body);
+    // console.log("JSON object returned: ", data); // testing output
 
-  // if no data returned by search, inform user
-  if (!data[0]) {
-    console.log(`Sorry, no results found for ${breed}.`)
+    // if no data returned by search, inform user
+    if (!data[0]) {
+      // pass null to error, message to results
+      callback(null, `Sorry, no results found for ${breedName}.`);
+      
+    // pass null to error, description to results
+    } else if (data[0].description) {
+      callback(null, data[0].description);
+    }
+  });
+};
 
-  // if data is returned, print it out
-  } else if (data[0].description) {
-    console.log(data[0].description);
-  }
-  
-});
+module.exports = fetchBreedDescription;
